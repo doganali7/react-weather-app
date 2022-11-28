6f50e212c4754f12e2b50b938e06ea0f8fc91270
Ali Dogan <alidogn98@gmail.com>
2022-11-28 05:09:26 +0200

    api call added

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import WeatherDetails from "./WeatherDetails";
 
 export default function SearchMain() {
   const [searchTerm, setSearchTerm] = useState("Nicosia");
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=5074ec189c637bcee3766de4820faede`;
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
   return (
     <>
       <div className="wrap">
@@ -16,7 +31,9 @@ export default function SearchMain() {
             onChange={(event) => setSearchTerm(event.target.value)}
           />
         </div>
-        <button className="searchButton">Search</button>
+        <button className="searchButton" onClick={getWeatherInfo}>
+          Search
+        </button>
       </div>
       <WeatherDetails />
     </>
--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -16,6 +16,49 @@ export default function WeatherDetails() {
             <div className="place">Nicosia, CY</div>
           </div>
         </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                6:30 PM <br />
+                Sunset
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                %60 <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                22 <br />
+                Pressure
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                10km/h <br />
+                Wind Speed
+              </p>
+            </div>
+          </div>
+        </div>
       </article>
     </>
   );
